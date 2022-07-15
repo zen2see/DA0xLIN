ad8cb5517680d669be37ad030fcc25527981d9f1
zen <zen2see@gmail.com>
2022-07-15 19:49:16 -0400

    UPDATED removed files unneeded components

--- packages/da0xlinapp/src/app/main/MainHeader.tsx
+++ packages/da0xlinapp/src/app/main/MainHeader.tsx
@@ -5,25 +5,24 @@ import Link from 'next/link'
 import { LedgerSigner } from '@ethersproject/hardware-wallets'
 import { getNetwork } from '@ethersproject/networks'
 import z2savatarSmaller from '/public/assets/images/z2savatarSmaller.png'
-import search from '/public/assets/icons/search.svg'
+
 
 export const MainHeader = () => {
   return (
     <>
-      <div className={`flex items-center justify-between ${styles.bgPrimary}`}>
+      <div className={`flex items-center justify-between ${styles.bgDkEmerald}`}>
         <div className={``}>
           <Link href='/'>
             <a>
               <Image className={`align-bottom`}
                 src={z2savatarSmaller}
                 alt='z2savatar'
-                width={42}
-                height={62}
+                width={36}
+                height={50}
               />
             </a>
           </Link>
         </div>
-  
       </div>
     </>
   )
