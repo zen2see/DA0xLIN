c240dedac76712de2db4bfb4c59cc076de83e251
zen <zen2see@gmail.com>
2022-10-05 01:13:24 -0400

    ADDED exports to componentsjsm

--- packages/da0xlinapp/src/pages/componentsjsm/index.ts
+++ packages/da0xlinapp/src/pages/componentsjsm/index.ts
@@ -1,8 +1,11 @@
 import Billing from './Billing'
 import Business from './Business'
+import Buttonjsm from './Buttonjsm'
 import CardDeal from './CardDeal'
 import Clients from './Clients'
 import CTA from './CTA'
+import FeedbackCard from './FeedbackCard'
+import GetStarted from './GetStarted'
 import Stats from './Stats'
 import Testimonials from './Testimonials'
 import Hero from './Hero'
@@ -10,10 +13,13 @@ import Hero from './Hero'
 export {
   Billing,
   Business,
-  Clients,
+  Buttonjsm,
   CardDeal,
+  Clients,
   CTA,
+  FeedbackCard,
+  GetStarted,
+  Hero,
   Stats,
   Testimonials,
-  Hero,
 }
